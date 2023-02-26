8d861ef1691efb6d920a398e2275022451f42d3a
Takeyuki UEDA <ueda@latelierdueda.com>
2023-02-26 18:51:24 +0900

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,45 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import analyze from "rollup-plugin-analyzer";
+import { visualizer } from 'rollup-plugin-visualizer';
+
+//
+// yarn build --mode analyze
+//
+// https://zenn.dev/manalink_dev/articles/vite-bundle-analyzer
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    lib: {
-      entry: '/src/components/build-entry.js',
-      name: 'qrcode-of-this-site',
-      fileName: (format) => `qrcode-of-this-site.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // Add external deps here
-        globals: {
-          vue: 'Vue',
+export default (({ mode }) => {
+  return {
+    build: {
+      lib: {
+        entry: '/src/components/build-entry.js',
+        name: 'qrcode-of-this-site',
+        fileName: (format) => `qrcode-of-this-site.${format}.js`,
+      },
+      rollupOptions: {
+        external: ['vue'],
+        output: {
+          // Provide global variables to use in the UMD build
+          // Add external deps here
+          globals: {
+            vue: 'Vue',
+          },
+          plugins: [
+            analyze(),
+            mode === 'analyze' && visualizer({
+              open: true,
+              filename: 'dist/stats.html',
+              gzipSize: true,
+              brotliSize: true,
+            })
+          ],
         },
-        plugins: [analyze()],
       },
     },
-  },
-/*  optimizeDeps: {
-    include: ["qrcode"],
-  },*/
-  plugins: [vue()],
+  /*  optimizeDeps: {
+      include: ["qrcode"],
+    },*/
+    plugins: [vue()],
+  }
 })
